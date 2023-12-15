ee50ac65a565796dd7fe2c7a3ad2d844f5ced7b7
Julietro29 <mousemiu1@gmail.com>
2023-12-15 23:42:35 +0200

    css

--- js/task-03.js
+++ js/task-03.js
@@ -1,10 +1,8 @@
 const nameInput = document.getElementById('name-input');
 const nameOutput = document.getElementById('name-output');
+
 nameInput.addEventListener('input', (event) => {
-const inputValue = event.target.value.trim();
-if (inputValue === '') {
-nameOutput.textContent = 'Anonymous';
-} else {
-nameOutput.textContent = inputValue;
-}
+  const inputValue = event.target.value.trim();
+  const outputValue = inputValue === '' ? 'Anonymous' : inputValue;
+  nameOutput.textContent = outputValue;
 });
